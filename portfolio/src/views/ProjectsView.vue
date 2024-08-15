6a06ae29c762074b0cc6287ddb296d86291106ba
dg0h <danellegoh.2021@scis.smu.edu.sg>
2024-08-15 23:50:44 +0800

    added project descriptions

--- portfolio/src/views/ProjectsView.vue
+++ portfolio/src/views/ProjectsView.vue
@@ -13,13 +13,14 @@
             <img class="projLogo" src="../assets/keppel.png">
           </div>
 
-          <div class="col-lg-7 col-md-6 col-sm-9">
+          <div class="col-lg-8 col-md-7 col-sm-9">
             <a class="content1 link" target="_blank" href="https://www.keppel.com/en/">Keppel Limited</a>
             <p class="content2">Enterprise Application Intern</p>
             <p class="content2">Jan 2024 - Aug 2024</p>
+            <p class="content4">Coordinated annual User Access Review and supported daily operations for multiple applications, including SAP, Electronic Invoicing System, Service Request Tool and Treasury Management System.</p>
           </div>
 
-          <div class="col-md-4">
+          <div class="col-md-3">
             <div class="logoContainer">
               <img class="skillLogo" src="../assets/mssql.png">
               <img class="skillLogo" src="../assets/html.png">
@@ -45,13 +46,14 @@
             <img class="projLogo" src="../assets/laptop.png">
           </div>
 
-          <div class="col-lg-7 col-md-6 col-sm-9">
+          <div class="col-lg-8 col-md-7 col-sm-9">
             <p class="content1">Portfolio</p>
             <p class="content2">Personal Project</p>
             <p class="content2">Jan 2024 - Present</p>
+            <p class="content4">Ongoing personal project to showcase what I have worked on previously.</p>
           </div>
 
-          <div class="col-md-4">
+          <div class="col-md-3">
             <div class="logoContainer">
               <img class="skillLogo" src="../assets/html.png">
               <img class="skillLogo" src="../assets/javascript.png">
@@ -70,13 +72,14 @@
             <img class="projLogo" src="../assets/lifebelowwater.png">
           </div>
 
-          <div class="col-lg-7 col-md-6 col-sm-9">
+          <div class="col-lg-8 col-md-7 col-sm-9">
             <p class="content1">Life Below Water</p>
             <p class="content2">Web Application Development | School Project</p>
             <p class="content2">Aug 2023 - Nov 2023</p>
+            <p class="content4">A web application aimed at increasing awareness and promoting action for marine conservation. It includes an interactive map, latest news, donation and volunteer pages, as well as a fun personality quiz.</p>
           </div>
 
-          <div class="col-md-4">
+          <div class="col-md-3">
             <div class="logoContainer">
               <img class="skillLogo" src="../assets/html.png">
               <img class="skillLogo" src="../assets/javascript.png">
@@ -95,13 +98,14 @@
             <img class="projLogo" src="../assets/allinone.png">
           </div>
 
-          <div class="col-lg-7 col-md-6 col-sm-9">
+          <div class="col-lg-8 col-md-7 col-sm-9">
             <p class="content1">All in One - Skill-Based Portal</p>
             <p class="content2">Software Project Management | School Project</p>
             <p class="content2">Aug 2023 - Nov 2023</p>
+            <p class="content4">Agile-managed project using scrum methodology. The application improves job application process within the organisation – for applicants, managers and HR.</p>
           </div>
 
-          <div class="col-md-4">
+          <div class="col-md-3">
             <div class="logoContainer">
               <img class="skillLogo" src="../assets/javascript.png">
               <img class="skillLogo" src="../assets/python.png">
@@ -120,13 +124,14 @@
             <img class="projLogo" src="../assets/ecopal.png">
           </div>
 
-          <div class="col-lg-7 col-md-6 col-sm-9">
+          <div class="col-lg-8 col-md-7 col-sm-9">
             <p class="content1">EcoPal</p>
             <p class="content2">Enterprise Solution Development | School Project</p>
             <p class="content2">Jan 2023 - Apr 2023</p>
+            <p class="content4">A web application to promote environmental sustainability, by reducing the use of single-use plastic bags through incentivisation and community-building.</p>
           </div>
 
-          <div class="col-md-4">
+          <div class="col-md-3">
             <div class="logoContainer">
               <img class="skillLogo" src="../assets/javascript.png">
               <img class="skillLogo" src="../assets/python.png">
@@ -155,13 +160,14 @@
             <img class="projLogo" src="../assets/timperio.png">
           </div>
 
-          <div class="col-lg-7 col-md-6 col-sm-9">
-            <a class="content1 link" target="_blank" href="https://timperio.co/">Timperio Case Study</a>
+          <div class="col-lg-8 col-md-7 col-sm-9">
+            <a class="content1 link" target="_blank" href="https://drive.google.com/file/d/1NBBqMygcb-pjqLFG1ZKe4ESlPmYYXJ2R/view?usp=sharing">Timperio Case Study</a>
             <p class="content2">Managing Customer Relations with Analytics | School Project</p>
             <p class="content2">Oct 2023 - Nov 2023</p>
+            <p class="content4">Provided an executive recommendation to improve Timperio's customer success strategy for the digital-online shopper, including creation of Tableau dashboard and mapping of customer journey.</p>
           </div>
 
-          <div class="col-md-4">
+          <div class="col-md-3">
             <div class="logoContainer">
               <img class="skillLogo" src="../assets/tableau.png">
             </div>
@@ -176,13 +182,14 @@
             <img class="projLogo" src="../assets/smug.png">
           </div>
 
-          <div class="col-lg-7 col-md-6 col-sm-9">
+          <div class="col-lg-8 col-md-7 col-sm-9">
             <p class="content1">SMUG - eLearn Mobile Companion</p>
             <p class="content2">Interaction Design & Prototyping | School Project</p>
             <p class="content2">Aug 2022 - Nov 2022</p>
+            <p class="content4">Prototype of a mobile application to notify users of upcoming academic deadlines and keep track of relevant course materials. Includes gamification aimed at improving effectiveness of study sessions.</p>
           </div>
 
-          <div class="col-md-4">
+          <div class="col-md-3">
             <div class="logoContainer">
               <img class="skillLogo" src="../assets/figma.png">
             </div>
@@ -197,13 +204,14 @@
             <img class="projLogo" src="../assets/sentimentanalysis.png">
           </div>
 
-          <div class="col-lg-7 col-md-6 col-sm-9">
+          <div class="col-lg-8 col-md-7 col-sm-9">
             <p class="content1">Sentiment Analysis of Legalising Recreational Marijuana in America</p>
             <p class="content2">Computational Social Science | School Project</p>
             <p class="content2">Aug 2022 - Nov 2022</p>
+            <p class="content4">Analysed online sentiments from Twitter and Reddit using relevant APIs, breaking down into general and state-specific analysis. Conducted VADER, text, log-odds, and empath analysis.</p>
           </div>
 
-          <div class="col-md-4">
+          <div class="col-md-3">
             <div class="logoContainer">
               <img class="skillLogo" src="../assets/python.png">
               <img class="skillLogo" src="../assets/twitter.png">
@@ -220,13 +228,14 @@
             <img class="projLogo" src="../assets/plainvanilla.png">
           </div>
 
-          <div class="col-lg-7 col-md-6 col-sm-9">
+          <div class="col-lg-8 col-md-7 col-sm-9">
             <a class="content1 link" target="_blank" href="https://plainvanilla.com.sg/">Plain Vanilla Product & Sales Analysis</a>
             <p class="content2">Spreadsheet Modeling & Analytics | School Project</p>
             <p class="content2">Jan 2022 - Apr 2022</p>
+            <p class="content4">Performance dashboard to analyse online sales data, forecasting of sales to optimise labour allocation, forecasting profit based on marketing strategies.</p>
           </div>
 
-          <div class="col-md-4">
+          <div class="col-md-3">
             <div class="logoContainer">
               <img class="skillLogo" src="../assets/excel.png">
             </div>
