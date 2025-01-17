c29fba7c5d51c4374bf1df829e62d9aafc5f131a
dg0h <danellegoh.2021@scis.smu.edu.sg>
2025-01-17 15:34:08 +0800

    updated projects and links

--- portfolio/src/views/ProjectsView.vue
+++ portfolio/src/views/ProjectsView.vue
@@ -8,29 +8,6 @@
       </div>
 
       <!-- Keppel Internship -->
-      <!-- <div class="container box">
-        <div class="row align-items-center justify-content-center">
-          <div class="col-lg-1 col-md-2 col-sm-3">
-            <img class="projLogo" src="../assets/keppel.png">
-          </div>
-
-          <div class="col-lg-8 col-md-7 col-sm-9">
-            <a class="content1 link" target="_blank" href="https://www.keppel.com/en/">Keppel Limited</a>
-            <p class="content2">Enterprise Application Intern</p>
-            <p class="content2">Jan 2024 - Aug 2024</p>
-            <p class="content4">Coordinated annual User Access Review and supported daily operations for multiple applications, including SAP, Electronic Invoicing System, Service Request Tool and Treasury Management System.</p>
-          </div>
-
-          <div class="col-md-3">
-            <div class="logoContainer">
-              <img class="skillLogo" src="../assets/mssql.png">
-              <img class="skillLogo" src="../assets/html.png">
-              <img class="skillLogo" src="../assets/javascript.png">
-            </div>
-          </div>
-        </div>
-      </div> -->
-
       <project
         :projectLogo="keppelLogo"
         companyLink="https://www.keppel.com/en/"
@@ -51,31 +28,6 @@
       </div>
 
       <!-- Portfolio -->
-      <!-- <div class="container box">
-        <div class="row align-items-center justify-content-center">
-          <div class="col-lg-1 col-md-2 col-sm-3">
-            <img class="projLogo" src="../assets/laptop.png">
-          </div>
-
-          <div class="col-lg-8 col-md-7 col-sm-9">
-            <p class="content1">Portfolio</p>
-            <p class="content2">Personal Project</p>
-            <p class="content2">Jan 2024 - Present</p>
-            <p class="content4">Ongoing personal project to showcase what I have worked on previously.</p>
-          </div>
-
-          <div class="col-md-3">
-            <div class="logoContainer">
-              <img class="skillLogo" src="../assets/html.png">
-              <img class="skillLogo" src="../assets/javascript.png">
-              <img class="skillLogo" src="../assets/css.png">
-              <img class="skillLogo" src="../assets/bootstrap.png">
-              <img class="skillLogo" src="../assets/vue.png">
-            </div>
-          </div>
-        </div>
-      </div> -->
-
       <project
         :projectLogo="laptopLogo"
         companyLink="https://danellegoh.netlify.app/"
@@ -90,51 +42,39 @@
       <project
         :projectLogo="h365Logo"
         companyLink="https://h365.vercel.app/"
-        projectName="Healthy 365 Prototype for Youth"
-        title="Final Year Project"
+        projectName="Healthy 365 for Youth"
+        title="Capstone Project"
         startDate="Aug 2024"
         endDate="Nov 2024"
-        description="Capstone project in collaboration with the Health Promotion Board (HPB), to create an improved version of H365 targeted at youth aged 13 to 17 years old, offering a gamified experience with non-monetary rewards."
+        description="Collaborated with the Health Promotion Board (HPB), to create an improved version of H365 targeted at youth aged 13 to 17 years old, offering a gamified experience with non-monetary rewards."
         :skillLogos="[pythonLogo, flaskLogo, postgresqlLogo, javascriptLogo, vueLogo]" />
 
       <!-- IS442 Timperio CRM -->
       <project
         :projectLogo="timperioLogo"
-        projectName="Customer Relationship Management (CRM) System"
+        companyLink="https://github.com/onebignick/is442-project"
+        projectName="Timperio Customer Relationship Management (CRM) System"
         title="Object Oriented Programming | School Project"
         startDate="Aug 2024"
         endDate="Nov 2024"
         description="The web application enables efficient visualisation of customer purchase histories and personalised marketing efforts. It also aims to enhance customer engagement, improve sales performance, and streamline the management of customer data."
         :skillLogos="[javaLogo, springbootLogo, postgresqlLogo, typescriptLogo, reactLogo]" /> 
       
+      <!-- To Do List Mobile App -->
+      <project
+        :projectLogo="laptopLogo"
+        companyLink="https://github.com/danellegoh/to-do-frontend"
+        projectName="To Do List Mobile Application"
+        title="Personal Project"
+        startDate="Nov 2024"
+        endDate="Nov 2024"
+        description="Experimental project to build a To Do List, with Android as the target OS, using React Native powered by Laravel backend."
+        :skillLogos="[phpLogo, laravelLogo, postgresqlLogo, typescriptLogo, reactLogo]" /> 
+
       <!-- IS216 Life Below Water -->
-      <!-- <div class="container box">
-        <div class="row align-items-center justify-content-center">
-          <div class="col-lg-1 col-md-2 col-sm-3">
-            <img class="projLogo" src="../assets/lifebelowwater.png">
-          </div>
-
-          <div class="col-lg-8 col-md-7 col-sm-9">
-            <p class="content1">Life Below Water</p>
-            <p class="content2">Web Application Development | School Project</p>
-            <p class="content2">Aug 2023 - Nov 2023</p>
-            <p class="content4">A web application aimed at increasing awareness and promoting action for marine conservation. It includes an interactive map, latest news, donation and volunteer pages, as well as a fun personality quiz.</p>
-          </div>
-
-          <div class="col-md-3">
-            <div class="logoContainer">
-              <img class="skillLogo" src="../assets/html.png">
-              <img class="skillLogo" src="../assets/javascript.png">
-              <img class="skillLogo" src="../assets/css.png">
-              <img class="skillLogo" src="../assets/bootstrap.png">
-              <img class="skillLogo" src="../assets/vue.png">
-            </div>
-          </div>
-        </div>
-      </div> -->
-
       <project
         :projectLogo="lifeBelowWaterLogo"
+        companyLink="https://github.com/L1uM4rt1n/WAD2_Project"
         projectName="Life Below Water"
         title="Web Application Development | School Project"
         startDate="Aug 2023"
@@ -143,33 +83,9 @@
         :skillLogos="[htmlLogo, javascriptLogo, cssLogo, bootstrapLogo, vueLogo]" />
 
       <!-- IS212 All in One -->
-      <!-- <div class="container box">
-        <div class="row align-items-center justify-content-center">
-          <div class="col-lg-1 col-md-2 col-sm-3">
-            <img class="projLogo" src="../assets/allinone.png">
-          </div>
-
-          <div class="col-lg-8 col-md-7 col-sm-9">
-            <p class="content1">All in One - Skill-Based Portal</p>
-            <p class="content2">Software Project Management | School Project</p>
-            <p class="content2">Aug 2023 - Nov 2023</p>
-            <p class="content4">Agile-managed project using scrum methodology. The application improves job application process within the organisation – for applicants, managers and HR.</p>
-          </div>
-
-          <div class="col-md-3">
-            <div class="logoContainer">
-              <img class="skillLogo" src="../assets/javascript.png">
-              <img class="skillLogo" src="../assets/python.png">
-              <img class="skillLogo" src="../assets/flask.png">
-              <img class="skillLogo" src="../assets/mysql.png">
-              <img class="skillLogo" src="../assets/vue.png">
-            </div>
-          </div>
-        </div>
-      </div> -->
-
       <project
         :projectLogo="allInOneLogo"
+        companyLink="https://github.com/junyang99/Skill_Based_portal"
         projectName="All in One - Skill-Based Portal"
         title="Software Project Management | School Project"
         startDate="Aug 2023"
@@ -178,32 +94,6 @@
         :skillLogos="[pythonLogo, flaskLogo, mysqlLogo, javascriptLogo, vueLogo]" />
 
       <!-- IS213 EcoPal -->
-      <!-- <div class="container box">
-        <div class="row align-items-center justify-content-center">
-          <div class="col-lg-1 col-md-2 col-sm-3">
-            <img class="projLogo" src="../assets/ecopal.png">
-          </div>
-
-          <div class="col-lg-8 col-md-7 col-sm-9">
-            <p class="content1">EcoPal</p>
-            <p class="content2">Enterprise Solution Development | School Project</p>
-            <p class="content2">Jan 2023 - Apr 2023</p>
-            <p class="content4">A web application to promote environmental sustainability, by reducing the use of single-use plastic bags through incentivisation and community-building.</p>
-          </div>
-
-          <div class="col-md-3">
-            <div class="logoContainer">
-              <img class="skillLogo" src="../assets/javascript.png">
-              <img class="skillLogo" src="../assets/python.png">
-              <img class="skillLogo" src="../assets/flask.png">
-              <img class="skillLogo" src="../assets/mysql.png">
-              <img class="skillLogo" src="../assets/rabbitmq.png">
-              <img class="skillLogo" src="../assets/docker.png">
-            </div>
-          </div>
-        </div>
-      </div> -->
-
       <project
         :projectLogo="ecopalLogo"
         projectName="EcoPal"
@@ -223,27 +113,6 @@
       </div>
 
       <!-- IS446 Timperio -->
-      <!-- <div class="container box">
-        <div class="row align-items-center justify-content-center">
-          <div class="col-lg-1 col-md-2 col-sm-3">
-            <img class="projLogo" src="../assets/timperio.png">
-          </div>
-
-          <div class="col-lg-8 col-md-7 col-sm-9">
-            <a class="content1 link" target="_blank" href="https://drive.google.com/file/d/1NBBqMygcb-pjqLFG1ZKe4ESlPmYYXJ2R/view?usp=sharing">Timperio Case Study</a>
-            <p class="content2">Managing Customer Relations with Analytics | School Project</p>
-            <p class="content2">Oct 2023 - Nov 2023</p>
-            <p class="content4">Provided an executive recommendation to improve Timperio's customer success strategy for the digital-online shopper, including creation of Tableau dashboard and mapping of customer journey.</p>
-          </div>
-
-          <div class="col-md-3">
-            <div class="logoContainer">
-              <img class="skillLogo" src="../assets/tableau.png">
-            </div>
-          </div>
-        </div>
-      </div> -->
-
       <project
         :projectLogo="timperioLogo"
         companyLink="https://drive.google.com/file/d/1NBBqMygcb-pjqLFG1ZKe4ESlPmYYXJ2R/view?usp=sharing"
@@ -255,29 +124,9 @@
         :skillLogos="[tableauLogo]" />
 
       <!-- IS211 SMUG -->
-      <!-- <div class="container box">
-        <div class="row align-items-center justify-content-center">
-          <div class="col-lg-1 col-md-2 col-sm-3">
-            <img class="projLogo" src="../assets/smug.png">
-          </div>
-
-          <div class="col-lg-8 col-md-7 col-sm-9">
-            <p class="content1">SMUG - eLearn Mobile Companion</p>
-            <p class="content2">Interaction Design & Prototyping | School Project</p>
-            <p class="content2">Aug 2022 - Nov 2022</p>
-            <p class="content4">Prototype of a mobile application to notify users of upcoming academic deadlines and keep track of relevant course materials. Includes gamification aimed at improving effectiveness of study sessions.</p>
-          </div>
-
-          <div class="col-md-3">
-            <div class="logoContainer">
-              <img class="skillLogo" src="../assets/figma.png">
-            </div>
-          </div>
-        </div>
-      </div> -->
-
       <project
         :projectLogo="smugLogo"
+        companyLink="https://www.figma.com/proto/33SdEPZMtDAv62nd9WFTMv/SMUG-Prototype?page-id=14%3A3971&node-id=225-4622&viewport=371%2C494%2C0.08&t=gpW2EsXm4ppxzftT-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=225%3A4622&show-proto-sidebar=1"
         projectName="SMUG - eLearn Mobile Companion"
         title="Interaction Design & Prototyping | School Project"
         startDate="Aug 2022"
@@ -286,29 +135,6 @@
         :skillLogos="[figmaLogo]" />
 
       <!-- SMT203 Marijuana -->
-      <!-- <div class="container box">
-        <div class="row align-items-center justify-content-center">
-          <div class="col-lg-1 col-md-2 col-sm-3">
-            <img class="projLogo" src="../assets/sentimentanalysis.png">
-          </div>
-
-          <div class="col-lg-8 col-md-7 col-sm-9">
-            <p class="content1">Sentiment Analysis of Legalising Recreational Marijuana in America</p>
-            <p class="content2">Computational Social Science | School Project</p>
-            <p class="content2">Aug 2022 - Nov 2022</p>
-            <p class="content4">Analysed online sentiments from Twitter and Reddit using relevant APIs, breaking down into general and state-specific analysis. Conducted VADER, text, log-odds, and empath analysis.</p>
-          </div>
-
-          <div class="col-md-3">
-            <div class="logoContainer">
-              <img class="skillLogo" src="../assets/python.png">
-              <img class="skillLogo" src="../assets/twitter.png">
-              <img class="skillLogo" src="../assets/reddit.png">
-            </div>
-          </div>
-        </div>
-      </div> -->
-
       <project
         :projectLogo="sentimentAnalysisLogo"
         projectName="Sentiment Analysis of Legalising Recreational Marijuana in America"
@@ -319,27 +145,6 @@
         :skillLogos="[pythonLogo, twitterLogo, redditLogo]" />
 
       <!-- COR1305 PV -->
-      <!-- <div class="container box">
-        <div class="row align-items-center justify-content-center">
-          <div class="col-lg-1 col-md-2 col-sm-3">
-            <img class="projLogo" src="../assets/plainvanilla.png">
-          </div>
-
-          <div class="col-lg-8 col-md-7 col-sm-9">
-            <a class="content1 link" target="_blank" href="https://plainvanilla.com.sg/">Plain Vanilla Product & Sales Analysis</a>
-            <p class="content2">Spreadsheet Modeling & Analytics | School Project</p>
-            <p class="content2">Jan 2022 - Apr 2022</p>
-            <p class="content4">Performance dashboard to analyse online sales data, forecasting of sales to optimise labour allocation, forecasting profit based on marketing strategies.</p>
-          </div>
-
-          <div class="col-md-3">
-            <div class="logoContainer">
-              <img class="skillLogo" src="../assets/excel.png">
-            </div>
-          </div>
-        </div>
-      </div> -->
-
       <project
         :projectLogo="plainVanillaLogo"
         companyLink="https://plainvanilla.com.sg/"
@@ -389,6 +194,8 @@ import typescriptLogo from '../assets/typescript.png';
 import reactLogo from '../assets/react.png';
 import javaLogo from '../assets/java.png';
 import springbootLogo from '../assets/springboot.png';
+import laravelLogo from '../assets/laravel.png';
+import phpLogo from '../assets/php.png';
 
 import project from '../components/project.vue';
 
@@ -429,7 +236,9 @@ export default {
       typescriptLogo,
       reactLogo,
       javaLogo,
-      springbootLogo
+      springbootLogo,
+      laravelLogo,
+      phpLogo
     }
   }
 }
